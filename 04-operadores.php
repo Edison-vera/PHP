f71de5d3d72b22f89eb25e80c4ef78f28c81403b
Edison-vera <edisonefvc15@gmail.com>
2022-02-25 18:55:28 -0500

    operators

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
 
 
+//Sumar
+
+echo $numero1 + $numero2;
+
+//resta
+
+echo $numero1 - $numero2;
+
+//Multiplicacion
+
+echo $numero1 * $numero2;
+
+//Division
+
+echo $numero1 / $numero2;
+
+//Multiplicar cierta cantidad de veces
+
+echo 2 ** 3;
 
 include 'includes/footer.php';
