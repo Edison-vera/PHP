983ad5d2378d78bba832afd462f7cecbedb41e02
Edison-vera <edisonefvc15@gmail.com>
2022-02-25 18:41:03 -0500

    Variable

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,17 @@
 <?php include 'includes/header.php';
 
+$nombre="Edison";
 
+echo $nombre;
+var_dump($nombre);
 
+define('constante',"este es el valor de la constante");
+echo constante;
+
+const constante2 = "hola 2";
+echo constante2;
+
+$nombreCliente="Pedro";
+$nombre_cliente="Pedro";
 
 include 'includes/footer.php';
