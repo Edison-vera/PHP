c62affd076b0b9d3b74f235880d17197dc1242f3
Edison-vera <edisonefvc15@gmail.com>
2022-02-24 19:19:07 -0500

    Hello World

---
+++ 01-hola-mundo.php
@@ -0,0 +1,16 @@
+<?php include "includes/header.php"; ?>
+
+<?php echo "Hello world"; ?>
+<br>
+
+<?php echo("Hello world");
+
+print("Hello world");
+
+print"Hello world";
+
+print_r("Hello world");
+
+var_dump("Hello world");
+
+include "includes/footer.php"; ?>
