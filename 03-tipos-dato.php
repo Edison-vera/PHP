f8c2a41f2b8df4c4fd9cb20000b69083a65b4f32
Edison-vera <edisonefvc15@gmail.com>
2022-02-25 18:50:19 -0500

    type of data

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,26 @@
 <?php include 'includes/header.php';
 
+//Boolean
 
+$logueado = true;
+var_dump($logueado);
 
+//Enteros
+$numero=200;
+var_dump($numero);
 
+//floats
+
+$float= 200.5;
+var_dump($float);
+
+//String
+
+$nombre ="Edison";
+var_dump($nombre);
+
+//array
+
+$array = [];
+var_dump($array);
 include 'includes/footer.php';
