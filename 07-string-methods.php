866e3feabd5eb6f3112f2dd7ce7b090997874b74
Edison-vera <edisonefvc15@gmail.com>
2022-02-25 19:40:20 -0500

    string methods

--- 07-string-methods.php
+++ 07-string-methods.php
@@ -1,6 +1,42 @@
 <?php include 'includes/header.php';
 
+$nombreCliente = "Edison Vera";
 
+// Conocer extension de un string
+echo strlen($nombreCliente);
+var_dump($nombreCliente);
+echo "<br/>";
 
+//Eliminar espacios en blanco 
+$texto = trim($nombreCliente);
+echo strlen($texto);
+echo "<br/>";
+
+//Convertirlo a mayusculas
+echo strtoupper($nombreCliente);
+echo "<br/>";
+
+//Concertirlo en minusculas
+echo strtolower($nombreCliente);
+echo "<br/>";
+
+$mail1 = "correo@correo.com";
+$mail2 = "Correo@correo.com";
+
+var_dump(strtolower($mail1) === strtolower($mail2));
+
+echo str_replace("Edison", "E",$nombreCliente);
+
+//Revisar si un string existe o no 
+echo strpos($nombreCliente,"Edison");
+
+//Contatenar string
+
+$tipoCliente = "Premium";
+echo "<br/>";
+
+echo "El cliente ". $nombreCliente , " es " .$tipoCliente;
+echo "<br/>";
+echo "El cliente {$nombreCliente} es {$tipoCliente} ";
 
 include 'includes/footer.php';
