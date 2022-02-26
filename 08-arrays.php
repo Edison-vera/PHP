c139bc5b37f1a9ff267cc21a663f9fd52e9509ac
Edison-vera <edisonefvc15@gmail.com>
2022-02-25 20:05:12 -0500

    arrays

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,5 +1,28 @@
 <?php include 'includes/header.php';
 
+$carrito = ["Tablet","Televisor","Computadora"];
+
+
+
+//Acceder a un elemento del array 
+echo($carrito[1]);
+
+//Añadir elementos en el arreglo 
+$carrito[3]= "Nuevo Producto...";
+
+//Añadir elementos al final del arreglo 
+
+array_push($carrito,"Audifonos");
+
+//Añadir elementos al inicio del arreglo 
+
+array_unshift($carrito,"Audifonos gamer");
+
+// Codigo util para ver los contenidos de los array
+echo "<pre>";
+var_dump($carrito);
+echo "<pre>";
+
 
 
 
--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,27 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    "nombre" => "Juan",
+    "saldo" => 200,
+    "informacion" => [
+        "tipo" => "premium"
+    ]
+];
 
+// Codigo util para ver los contenidos de los array
+echo "<pre>";
+var_dump($cliente["informacion"]);
+echo "<pre>";
+
+echo $cliente["nombre"];
+echo $cliente["informacion"]["tipo"];
+
+
+$cliente["codigo"] = 120011121;
+
+echo "<pre>";
+var_dump($cliente);
+echo "<pre>";
 
 
 include 'includes/footer.php';
