34aa5bd02e5a8361aa93f38e4740fee3db5051df
Edison-vera <edisonefvc15@gmail.com>
2022-02-28 19:44:43 -0500

    conditionals

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,71 @@
 <?php include 'includes/header.php';
 
+$autenticado = true;
+$admin = false;
+
+if ($autenticado || $admin){
+echo("Usuario autenticado correctamente");
+}else{
+    echo("Usuario no autenticado correctamente");
+}
+
+// If anidados ....
+
+$cliente =[
+    "nombre" => "Juan",
+    "saldo" => 0,
+    "informacion" => [
+        "tipo" => "premium"
+    ]
+    ];
+
+    echo "<pre>";
+
+if (!empty($cliente)){
+    echo("El arreglo del cliente no esta vacio");
 
 
+    if ($cliente["saldo"] > 0){
+        echo("El saldo del clientes esta disponible"); 
+    } else {
+        echo("No hay saldo"); 
+    }
+}
+
+
+// else if
+echo "<pre>";
+if($cliente["saldo"] > 0){
+    echo("Saldo disponible");
+} else if ($cliente["informacion"]["tipo"] === "premium"){
+    echo("El cliente es premium");
+} else {
+    echo("No hay cliente definido ");
+}
+
+
+// Switch
+echo "<pre>";
+$tecnologia = "HTML";
+
+switch($tecnologia){
+    case "PHP":
+        echo("PHP, un exelente lenguaje");
+        break;
+
+        case "Js":
+            echo("Js, El lenguaje de la web");
+            break;
+
+            case "HTML":
+                echo("Emmm....");
+                break;
+    
+
+        default:
+        echo("Algun lenguaje que no se cual es ");
+        break;
+}
 
 
 include 'includes/footer.php';
--- 13-loops.php
+++ 13-loops.php
@@ -1,6 +1,71 @@
 <?php include 'includes/header.php';
 
+// While
+
+$i = 0; // Inicializador
+
+while($i<10){
+
+    echo $i . "<br>";
+    $i++; //Incrementando uno a uno
+}
+
+// Do while
+
+echo "<pre>";
+
+$i=0;
+
+do{
+    echo $i . "<br>";
+    $i++;
+}while($i<10);
+
+// For lopp
+// Multiplos de 3 imprimir fizz
+// Multiplos 5 imprimir buzz
+// Multiplos 3 y 5 imprimir fizz buzz
 
 
+
+echo "<pre>";
+
+for($i=1; $i<10; $i++){
+    if($i % 3 === 0 && $i % 5 === 0){
+    echo $i . "fizz buzz <br>";
+}elseif ($i % 3 === 0){
+    echo $i . "fizz <br>";
+} elseif ($i % 5 ===0){
+    echo $i . "buzz <br>";
+}else {
+echo$i ."<br>";
+}
+}
+
+// For Each
+
+
+$clientes= ["Pedro","Juan","Karen"];
+echo "<pre>";
+foreach($clientes as $cliente){
+echo($cliente . "<br>" );
+}
+
+
+$cliente =[
+    "nombre" => "Juan",
+    "saldo" => 200,  
+    "tipo" => "premium" 
+    ];
+
+echo "<pre>";
+foreach($cliente as $valor){
+        echo($valor . "<br>" );
+        }
+
+echo "<pre>";      
+foreach($cliente as $key => $valor){
+            echo($key . "-" . $valor . "<br>" );
+            }
 
 include 'includes/footer.php';
