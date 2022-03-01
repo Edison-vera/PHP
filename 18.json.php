44c6dc184d736513d1e20a221f9049f4dbcc7541
Edison-vera <edisonefvc15@gmail.com>
2022-03-01 18:59:56 -0500

    Json

--- 18.json.php
+++ 18.json.php
@@ -1,7 +1,34 @@
 <?php include 'includes/header.php';
 
+$productos=[
+    [
+         "nombre" => "Tablet",
+         "precio" => 200,
+         "disponible" => true
+    ],
+    [
+        "nombre" => "Televisor 24 pulgadas",
+        "precio" => 300,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Monitor curvo",
+        "precio" => 400,
+        "disponible" => false
+            ]
+    ];
+
+echo "<pre>";
+var_dump($productos);
+
+$json = json_encode($productos);
+
+$json_arrys = json_decode($json);
 
 
+var_dump($json);
+var_dump($json_arrys);
+echo "<pre>";
 
 
 
