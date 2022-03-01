24f7f146cae3bf4fd2be1689d2fc3890a51cb88b
Edison-vera <edisonefvc15@gmail.com>
2022-02-28 20:09:38 -0500

    Foreach

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,34 @@
 <?php include 'includes/header.php';
 
+$productos=[
+    [
+"nombre" => "Tablet",
+"precio" => 200,
+"disponible" => true
+    ],
+    [
+        "nombre" => "Televisor 24 pulgadas",
+        "precio" => 300,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Monitor curvo",
+        "precio" => 400,
+        "disponible" => false
+            ]
+    ];
 
+
+foreach ($productos as $producto){ ?>
+    <li>
+<p>Producto: <?php	echo $producto["nombre"];?> </p>
+<p>Producto: <?php 	echo "$ " . $producto["precio"];?> </p>
+<p><?php echo($producto["disponible"]) ? "Disponible" : "No disponible"; ?></p>    
+    
+  
+</li>
+<?php
+}
 
 
 include 'includes/footer.php';
